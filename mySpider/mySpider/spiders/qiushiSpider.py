76b967d82cda245ceb42eb843e5faa8c70bd953b
longyin <longyincug@163.com>
2018-02-25 19:38:15 +0800

    Scrapy

--- mySpider/mySpider/spiders/qiushiSpider.py
+++ mySpider/mySpider/spiders/qiushiSpider.py
@@ -1,4 +1,5 @@
 import scrapy
+from mySpider.items import MyspiderItem
 
 class QiushiSpider(scrapy.Spider):
     name = 'qiushi'
@@ -6,9 +7,21 @@ class QiushiSpider(scrapy.Spider):
     start_urls = ['https://www.qiushibaike.com/']
 
     def parse(self, response):
+        content_list = [] 
         # 不加extract()结果为xpath匹配对象
         for each in response.xpath('//div[contains(@class,"article block untagged mb15")]'):
+            item = MyspiderItem() 
             name = each.xpath('.//h2/text()')[0].extract()
             img = each.xpath('./div[@class="author clearfix"]//img/@src')[0].extract()
             content = each.xpath('.//div[@class="content"]/span/text()')[0].extract()
-            print(name, img, content)
+            #print(name, img, content)
+            
+            item['name'] = name
+            item['img'] = img
+            item['content'] = content
+            content_list.append(item)
+
+        return content_list
+            
+
+
